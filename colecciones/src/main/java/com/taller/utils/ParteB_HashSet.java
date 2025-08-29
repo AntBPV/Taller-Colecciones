2a29ddf0eabb1518041b9e6a436711cda2e9f6a2
33sebas56 <sebas-ramos2018@outlook.com>
2025-08-28 19:27:16 -0500

    parte_b

---
+++ colecciones/src/main/java/com/taller/utils/ParteB_HashSet.java
@@ -0,0 +1,31 @@
+package com.taller.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+import com.taller.models.Pedido;
+
+public class ParteB_HashSet {
+
+    public static void ejecutar() {
+        System.out.println("=== PARTE B - CONJUNTO (HashSet) ===");
+
+        Set<Pedido> pedidosSet = new HashSet<>();
+
+        pedidosSet.add(new Pedido("Cafe", 14));
+        pedidosSet.add(new Pedido("Amper", 4));
+        pedidosSet.add(new Pedido("Tampico", 10));
+        pedidosSet.add(new Pedido("Helado", 2));
+        pedidosSet.add(new Pedido("Gelada", 1));
+
+        pedidosSet.add(new Pedido("Cafe", 14));
+        pedidosSet.add(new Pedido("Tampico", 10));
+
+        System.out.println("Contenido del HashSet:");
+        for (Pedido pedido : pedidosSet) {
+            System.out.println(pedido.getnombre_pedido() + " - $" + pedido.getPrecio());
+        }
+
+        System.out.println("Los duplicados se agregaron porque son objetos diferentes en memoria.");
+        System.out.println("Total de elementos: " + pedidosSet.size());
+    }
+}
